e8b5eda0b0fa81c28c329966131c6fcf8c0b040c
Tobias Kappé <tobias.kappe@gmail.com>
2018-12-19 17:05:26 +0000

    Solution to exercise 18 for 2018.

---
+++ 2018/18b.hs
@@ -0,0 +1,113 @@
+import Data.Map.Strict as M
+import Data.List as L
+import System.IO (isEOF)
+import Data.Maybe (catMaybes)
+
+
+data Acre = Open | Trees | Lumberyard deriving (Show, Eq)
+
+type Scan = Map (Int, Int) Acre
+
+
+neighbourhood :: Scan -> (Int, Int) -> [Acre]
+
+neighbourhood scan (x, y) =
+    let positions = [(x  , y+1),
+                     (x+1, y+1),
+                     (x+1, y  ),
+                     (x+1, y-1),
+                     (x  , y-1),
+                     (x-1, y-1),
+                     (x-1, y  ),
+                     (x-1, y+1)]
+        acres = L.map ((flip M.lookup) scan) positions
+    in catMaybes acres
+
+
+count :: Eq a => [a] -> a -> Int
+
+count xs x =
+    length $ L.filter (== x) xs
+
+
+next :: Scan -> (Int, Int) -> Acre -> Acre
+
+next scan (x, y) acre =
+    let hood = neighbourhood scan (x, y)
+    in case scan M.! (x, y) of
+           Open       -> if count hood Trees >= 3
+                         then Trees
+                         else Open
+           Trees      -> if count hood Lumberyard >= 3
+                         then Lumberyard
+                         else Trees
+           Lumberyard -> if count hood Lumberyard >= 1 && 
+                            count hood Trees >= 1
+                         then Lumberyard
+                         else Open
+
+
+tick :: Scan -> Scan
+
+tick scan =
+    M.mapWithKey (next scan) scan
+
+
+resourceValue scan =
+    let acres = M.elems scan
+    in (count acres Trees) * (count acres Lumberyard)
+
+
+findCycle scan =
+    let tortoises = iterate (tick) scan
+        hares = iterate (tick . tick) scan
+        Just (meet, _) = L.find (uncurry (==)) $ tail (zip tortoises hares)
+        newHares = iterate (tick) meet
+        Just mu = L.findIndex (uncurry (==)) (zip tortoises newHares)
+        (muVal : remainder) = L.drop (mu+1) tortoises
+        Just lam = L.findIndex (== muVal) remainder
+    in (mu, lam+1)
+
+
+readLines = do
+    done <- isEOF
+    if done
+    then return []
+    else do
+        line <- getLine
+        rest <- readLines
+        return (line : rest)
+
+
+parseLine :: Int -> Scan -> String -> Scan
+
+parseLine row scan line =
+    parseChars' row 0 scan line
+    where parseChars' row column scanned [] = scanned
+          parseChars' row column scanned (char : chars) =
+              let acre = case char of
+                             '.' -> Open
+                             '|' -> Trees
+                             '#' -> Lumberyard
+                  scan = M.insert (column, row) acre scanned
+              in parseChars' row (column+1) scan chars
+
+
+parseLines :: [String] -> Scan
+
+parseLines lines =
+    parseLines' 0 M.empty lines
+    where parseLines' row scanned [] = scanned
+          parseLines' row scanned (line : lines) =
+              let scan = parseLine row scanned line
+              in parseLines' (row+1) scan lines
+    
+
+
+main = do
+    lines <- readLines
+    let scan = parseLines lines
+        (mu, lam) = findCycle scan
+        jump = 1000000000 `mod` lam
+        final = iterate (tick) scan !! (jump+mu)
+    print $ resourceValue final
